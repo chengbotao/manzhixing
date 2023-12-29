2b385621c21dd5ddbbc05682d084225a91497b71
chengbotao <chengbotao5221@163.com>
2023-12-29 15:06:17 +0800

    docs(utilsxy): update isxxx docs

--- packages/utilsxy/src/isxxx/index.ts
+++ packages/utilsxy/src/isxxx/index.ts
@@ -3,14 +3,19 @@
  * @Contact: https://github.com/chengbotao
  */
 
+// #region isURLSearchParams
 export function isURLSearchParams(val: unknown): val is URLSearchParams {
   return typeof val !== 'undefined' && val instanceof URLSearchParams;
 }
+// #endregion isURLSearchParams
 
+// #region isFormData
 export function isFormData(val: unknown): val is FormData {
   return typeof val !== 'undefined' && val instanceof FormData;
 }
+// #endregion isFormData
 
+// #region isEmpty
 export function isEmpty(values: unknown[] | Record<string, unknown>): boolean {
   if (Array.isArray(values)) {
     // 数组为空
@@ -23,7 +28,10 @@ export function isEmpty(values: unknown[] | Record<string, unknown>): boolean {
   }
   return true;
 }
+// #endregion isEmpty
 
+// #region isPlainObject
 export function isPlainObject(val: unknown): val is Record<string, unknown> {
   return Object.prototype.toString.call(val) === '[object Object]';
 }
+// #endregion isPlainObject
